614f844b92bf7b45535a76c708238be7e3b347e6
AudreyErza <audrey.revenault@gmail.com>
2024-06-20 15:09:03 +0200

    Correction du warning post soutenance

--- kasa/src/components/Header/header.jsx
+++ kasa/src/components/Header/header.jsx
@@ -4,18 +4,25 @@ import './header.scss'
  
 function Header() {
     return (
-<header className="header">
-<img src={logo} alt="" className="header_logo" />
-<nav className="header_nav">
-    <NavLink to="/" className="header_link" activeclassName="header_link_active">Accueil</NavLink>
-    <NavLink to="/about" className="header_link" activeclassName="header_link_active">A propos</NavLink>
-        </nav>
-</header>
-        
+        <header className="header">
+            <img src={logo} alt="" className="header_logo" />
+            <nav className="header_nav">
+                <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "header_link_active" : "header_link"}
+                >
+                    Accueil
+                </NavLink>
+                <NavLink 
+                    to="/about" 
+                    className={({ isActive }) => isActive ? "header_link_active" : "header_link"}
+                >
+                    A propos
+                </NavLink>
+            </nav>
+        </header>
     )
 }
 
 export default Header
-
-
 
